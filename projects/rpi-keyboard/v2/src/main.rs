6f902a30771e4f98aeeef9a1689ad9f313a04bc5
kamanji <maxiartem969@gmail.com>
2018-12-12 21:00:49 +0300

    [REPO|WIP] Update ignore rules

--- projects/rpi-keyboard/v2/src/main.rs
+++ projects/rpi-keyboard/v2/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Key {
+    name: String,
+    code: i32,
+    first_repeat: i32,
+    repeat_after: i32,
 }
+
+impl Default for Key {
+    fn default() -> Key {
+        Key {
+            name: String::from("none"),
+            code: 0,
+            first_repeat: -1,
+            repeat_after: -1,
+        }
+    }
+}
+
+fn main() {}
