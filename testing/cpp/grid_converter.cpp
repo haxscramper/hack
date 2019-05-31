6206a6f12f045cc65c0c7c4aa560e94c10bacd6b
haxscramper <hax-scamper-git@protonmail.com>
2019-05-31 16:56:46 +0300

    [REFACTOR]{CPP} Add bounded increment/decrement functions

--- testing/cpp/grid_converter.cpp
+++ testing/cpp/grid_converter.cpp
@@ -1,11 +1,11 @@
 #include "../../common/cpp/macro.hpp"
 #include "../../common/cpp/using.hpp"
-#include "signals_slots.hpp"
-#include <vector>
 
-const char fill_char   = '0';
-const char path_char   = '1';
-const char change_char = 'F';
+// Include and declarations were made in the middle of include to
+// provide operator overloading for logging macro
+#include <iostream>
+#include <string>
+#include <vector>
 
 template <class T>
 using Vec  = std::vector<T>;
@@ -17,8 +17,45 @@ struct Pos {
     size_t col;
 };
 
+std::ostream& operator<<(std::ostream& os, const Grid& grid) {
+    os << "[ " << grid.size() << ", " << grid.at(0).size() << " ]";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Pos& pos) {
+    os << "( " << pos.row << ", " << pos.col << " )";
+    return os;
+}
+
+#include "signals_slots.hpp"
+
+const char fill_char   = '0';
+const char path_char   = '1';
+const char change_char = 'F';
+
+
 int min(int a, int b) {
     return (a < b ? a : b);
+}
+
+size_t row_count(const Grid& grid) {
+    return grid.size();
+}
+
+size_t col_count(const Grid& grid) {
+    return grid.at(0).size();
+}
+
+void bound_decrement(size_t& num) {
+    if (num > 0) {
+        --num;
+    }
+}
+
+void bound_increment(size_t& num, size_t max) {
+    if (num < max) {
+        ++num;
+    }
 }
 
 
@@ -40,7 +77,7 @@ Grid generate_grid(uS grid_cols, uS grid_rows = 0) {
     size_t dir = 1;
     // down: 0, forward: 1,3, up: 2
 
-    LOG << "Grid:" << grid.size() << grid[0].size();
+    LOG << "Grid:" << grid;
 
     size_t ctx = 0;
     while (pos.col < grid_cols) {
@@ -52,36 +89,10 @@ Grid generate_grid(uS grid_cols, uS grid_rows = 0) {
             // LOG << "Pos:" << pos.row << pos.col << "Dir:" << dir;
             grid.at(pos.row).at(pos.col) = path_char;
             switch (dir) {
-                case 0: {
-                    if (pos.row > 0) {
-                        pos.row -= 1;
-                    }
-                    break;
-                }
-
-                case 1: { // forward
-                    if (pos.col < grid_cols - 1) {
-                        pos.col += 1;
-                    }
-
-                    break;
-                }
-
-                case 2: {
-                    if (pos.row < grid_rows - 1) {
-                        pos.row += 1;
-                    }
-                    break;
-                }
-
-                case 3: { // forward
-                    if (pos.col < grid_cols - 1) {
-                        pos.col += 1;
-                    }
-
-                    break;
-                }
-
+                case 0: bound_decrement(pos.row); break;
+                case 1: bound_increment(pos.col, grid_cols - 1); break;
+                case 2: bound_increment(pos.row, grid_rows - 1); break;
+                case 3: bound_increment(pos.col, grid_cols - 1); break;
                 default: INFO << "Wrong dir " << dir; break;
             }
         }
@@ -117,18 +128,22 @@ class Writer : public signal_base
 
   public:
     void slot_write_grid(signal_msg data) {
-        Grid& grid = scastp<Grid>(data);
+        if (true) {
+            Grid& grid = scastp<Grid>(data);
 
-        for (Str& row : grid) {
-            printf("    ");
-            for (char c : row) {
-                switch (c) {
-                    case path_char: printf("\033[32m%c\033[0m", c); break;
-                    case change_char: printf("%c", c); break;
-                    default: printf("%c", c); break;
+            for (Str& row : grid) {
+                printf("    ");
+                for (char c : row) {
+                    switch (c) {
+                        case path_char:
+                            printf("\033[32m%c\033[0m", c);
+                            break;
+                        case change_char: printf("%c", c); break;
+                        default: printf("%c", c); break;
+                    }
                 }
+                printf("\n");
             }
-            printf("\n");
         }
     }
 };
@@ -151,7 +166,6 @@ class Stepper : public signal_base
         CheckRequest request = {pos, grid};
 
         signal_check_cell(&request);
-
         signal_done_search(&grid);
     }
 
@@ -160,11 +174,13 @@ class Stepper : public signal_base
     }
 
     void signal_check_cell(signal_msg request) {
-        Pos pos = scastp<CheckRequest>(request).pos;
+        Pos   pos  = scastp<CheckRequest>(request).pos;
+        Grid& grid = scastp<CheckRequest>(request).grid;
 
         LOG << "signal_check_cell"
-            << "Pos: " << pos.row << pos.col;
-        emit_signal(signal_cast(&Stepper::signal_check_cell), &request);
+            << "Pos: " << pos << "Grid:" << grid << grid.at(0).size();
+
+        emit_signal(signal_cast(&Stepper::signal_check_cell), request);
     }
 
     void slot_found_match(signal_msg msg) {
@@ -195,8 +211,6 @@ class Checker : public signal_base
 
 
     void slot_check_cell(signal_msg msg) {
-        LOG << "slot_check_cell";
-
         CheckRequest request = scastp<CheckRequest>(msg);
 
 
@@ -205,27 +219,13 @@ class Checker : public signal_base
 
 
         switch (direction) {
-            case Forward: {
-                pos.col += 1;
-                break;
-            }
-
-            case Down: {
-                pos.row -= 1;
-                break;
-            }
-
-            case Up: {
-                pos.row += 1;
-                break;
-            }
+            case Forward: bound_increment(pos.col, col_count(grid)); break;
+            case Down: bound_decrement(pos.row); break;
+            case Up: bound_increment(pos.row, row_count(grid)); break;
         }
 
 
         request.pos = pos;
-
-        LOG << grid.size();
-
 
         if (grid.at(pos.row).at(pos.col) == path_char) {
             LOG << "Found match";
@@ -239,12 +239,10 @@ class Checker : public signal_base
     }
 
     void signal_found_match(signal_msg request) {
-        LOG << "Signal found match";
         emit_signal(signal_cast(&Checker::signal_found_match), request);
     }
 
     void signal_missed_match(signal_msg request) {
-        LOG << "Signal missed match";
         emit_signal(signal_cast(&Checker::signal_missed_match), request);
     }
 
@@ -305,7 +303,7 @@ int main() {
     CONNECT_STEPPER_TO_CHECKER(down, missed_match)
     CONNECT_STEPPER_TO_CHECKER(forward, missed_match)
 
-    Grid grid = generate_grid(100, 30);
+    Grid grid = generate_grid(30, 10);
 
     reader.set_grid(grid);
 
