af4cee4c227b878d9b07f2c88d23bb83739d5fdb
haxscramper <hax-scamper-git@protonmail.com>
2019-07-18 18:44:27 +0300

    [FEATURE]{BOOST} Graphml read/write test

--- testing/cpp/boost_graph/write_graphml_struct_prop.cpp
+++ testing/cpp/boost_graph/write_graphml_struct_prop.cpp
@@ -1,5 +1,7 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graphml.hpp>
+#include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -8,7 +10,7 @@ using namespace boost;
 using Str = std::string;
 
 struct ActualVertexProp {
-    Str name;
+    Str name = "default name";
 };
 
 struct VertexProp {
@@ -41,21 +43,58 @@ using VertBundledType = vertex_bundle_type<Graph>::type;
 using EdgeBundledType = edge_bundle_type<Graph>::type;
 
 int main() {
-    Graph graph;
+    auto file_name = "graph.htodo_graph";
 
-    { // Describe graph
-        VertDesc vd_1 = add_vertex(graph);
-        VertDesc vd_2 = add_vertex(graph);
-        VertDesc vd_3 = add_vertex(graph);
+    {
+        Graph graph;
 
-        add_edge(vd_1, vd_2, "first_edge_property", graph);
-        add_edge(vd_1, vd_3, "second_edge_property", graph);
-        add_edge(vd_3, vd_2, "third_edge_property", graph);
+        { // Describe graph
+            VertDesc vd_1 = add_vertex(graph);
+            VertDesc vd_2 = add_vertex(graph);
+            VertDesc vd_3 = add_vertex(graph);
+
+            add_edge(vd_1, vd_2, "first_edge_property", graph);
+            add_edge(vd_1, vd_3, "second_edge_property", graph);
+            add_edge(vd_3, vd_2, "third_edge_property", graph);
+        }
+
+        dynamic_properties properties;
+        properties.property(
+            "vertex_property", get(&VertexProp::prop, graph));
+        properties.property("edge_property", get(&EdgeProp::name, graph));
+
+        {
+            std::ofstream out_file(file_name);
+            write_graphml(out_file, graph, properties, true);
+            out_file.close();
+        }
     }
 
-    dynamic_properties properties;
-    properties.property("vertex_property", get(&VertexProp::prop, graph));
-    properties.property("edge_property", get(&EdgeProp::name, graph));
+    {
+        Graph graph;
+        {
+            dynamic_properties properties;
+            properties.property(
+                "vertex_property", get(&VertexProp::prop, graph));
+            properties.property(
+                "edge_property", get(&EdgeProp::name, graph));
+            std::ifstream in_file(file_name);
+            if (in_file.is_open()) {
+                read_graphml(in_file, graph, properties);
+            } else {
+                puts("failed to open file");
+            }
+            remove(file_name);
+        }
 
-    write_graphml(std::cout, graph, properties, true);
+        {
+            dynamic_properties properties;
+            properties.property(
+                "vertex_property", get(&VertexProp::prop, graph));
+            properties.property(
+                "edge_property", get(&EdgeProp::name, graph));
+            write_graphml(std::cout, graph, properties, true);
+        }
+    }
+    puts("done main");
 }
