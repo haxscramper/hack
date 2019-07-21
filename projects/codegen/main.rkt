f1b118b1e6d41b8204831fd383e0e1900b08e0eb
haxscramper <hax-scamper-git@protonmail.com>
2019-07-21 13:14:56 +0300

    [FEATURE|REFACTOR]{RKT} Enum generation test

--- projects/codegen/main.rkt
+++ projects/codegen/main.rkt
@@ -1,24 +1,12 @@
 #! /usr/bin/env racket
 #lang typed/racket
 
-(struct code
-  ([statements : (Listof String)]))
-
-(struct CStatement
-  ([code : (Listof CNode)]))
-
 (struct CNode
   ([code : (Listof (U CNode String))]))
 
 (struct variable
   ([name : String]
    [type : String]))
-
-(struct function
-  ([name : String]
-   [args : (Listof variable)]
-   [return : String]))
-
 
 (define (make-CNode [s : String]) (CNode (list s)))
 (define (% [s : String]) (CNode (list s)))
@@ -101,58 +89,12 @@
    [node : CNode]) : String
   (string-join
    (map
-    (lambda
-        ([var : (U CNode String)])
+    (lambda ([var : (U CNode String)])
       (cond
         [(CNode? var) (CNode->code var)]
         [(string? var) var]))
     (CNode-code node))
    "\n"))
-
-
-
-(define
-  (code->c++-code
-   [c : code]) : String
-  (string-append
-   (string-join (code-statements c) "\n")
-   "\n"))
-
-(define
-  (get-signature [f : function]) : String
-  (string-append
-   (function-name f)
-   " : "
-   (string-join
-    (map
-     (lambda ([var : variable]) (variable-type var))
-     (function-args f))
-    " X ")
-   " |-> "
-   (function-return f)))
-
-(define
-  (function+code->c++-function
-   [f : function]
-   [c : code]) : String
-  (string-append
-   (function-return f)
-   " "
-   (function-name f)
-   "("
-   (string-join
-    (map
-     (lambda ([var : variable])
-       (string-append
-        (variable-type var)
-        " "
-        (variable-name var)))
-     (function-args f))
-    ", ")
-   ") {\n"
-   (code->c++-code c)
-   "}"))
-
 
 (let ([priorities
        '("Undefined"
@@ -171,7 +113,14 @@
      "QSTodo"
      '("DataItem")
      (list
-      (make-enum "Priority" priorities))))
+      (make-enum
+       "Priority"
+       priorities)
+      (make-function
+       "Priority"
+       "strToPriority"
+       (list (variable "str" "const std::string&"))
+       (list (% "return 0;"))))))
 
    (open-output-file
     "parse.cpp"
