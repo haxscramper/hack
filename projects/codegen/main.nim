a6d7d90d7d5e9816c77214d693eb59572f73b29c
haxscramper <hax-scamper-git@protonmail.com>
2019-07-22 17:01:36 +0300

    [FEATURE|REFACTOR]{NIM} C++ codegen
    + class and function code generation

--- projects/codegen/main.nim
+++ projects/codegen/main.nim
@@ -14,14 +14,8 @@ type
     props: seq[TypeProps]
 
   Var = object
-    vName: string
-    vType: Type
-
-  InhType = enum
-    public
-    private
-    protected
-
+    name: string
+    vtyp: Type
 
 type
   AcnKind = enum
@@ -34,40 +28,70 @@ type
       of acnEnum:
         vFields: seq[Var]
       of acnClass:
-        parents: seq[(InhType, Type)]
+        parents: seq[(string, string)]
         body: seq[Acn]
       of acnFunction:
         restype: Type
-        args: seq[(Var)]
+        args: seq[Var]
 
 type
-  CNodeKind = enum
-    cnkLeaf
-    cnkNode
   CNode = ref object
-    case kind: CNodeKind
-      of cnkLeaf:
-        code: string
-      of cnkNode:
-        under: seq[CNode]
-
-
-proc class_to_cnode(class: Acn): CNode =
-  CNode(
-    kind: cnkNode,
-    under: @[
-      CNode(
-        kind: cnkLeaf,
-        code: &"{class.name}"
-      )])
+    code: string
+    under: seq[CNode]
 
 proc cnode_to_string(cnode: CNode): string =
-  case cnode.kind:
-    of cnkLeaf: cnode.code
-    of cnkNode: join(map(cnode.under, cnode_to_string), "\n")
+  cnode.code & " " & join(map(cnode.under, cnode_to_string), "\n")
+
+proc acn_to_cnode(acn: Acn): CNode
+
+proc acn_class_to_cnode(acn: Acn): CNode =
+  CNode(
+    code: &"class {acn.name}",
+    under: concat(
+      if acn.parents.len == 0:
+        @[CNode(code: "{")]
+      else:
+        @[CNode(
+          code: " : " &
+            join(
+              map(
+                acn.parents,
+                proc(par: (string, string)): string =
+                  par[0] & " " & par[1] & " "),
+              " , ")),
+          CNode(code: "{")],
+      map(acn.body, acn_to_cnode),
+      @[CNode(code: "};")]))
+
+proc acn_enum_to_cnode(acn: Acn): CNode =
+  CNode(code: &"enum class {acn.name}")
+
+proc acn_function_to_cnode(acn: Acn): CNode =
+  CNode(
+    under: @[
+      CNode(code: acn.restype.name),
+      CNode(code: acn.name)])
 
 
-echo cnode_to_string(class_to_cnode(Acn(
+proc acn_to_cnode(acn: Acn): CNode =
+  CNode(
+    code: "",
+    under: @[
+      case acn.kind:
+        of acnClass: acn_class_to_cnode(acn)
+        of acnEnum: acn_enum_to_cnode(acn)
+        of acnFunction: acn_function_to_cnode(acn)
+  ])
+
+
+
+echo cnode_to_string(acn_to_cnode(Acn(
   kind: acnClass,
-  name: "QSTodo"
-)))
+  name: "QSTodo",
+  parents: @[("public", "qde::DataItem")],
+  body: @[Acn(
+    kind: acnFunction,
+    name: "hello",
+    restype: Type(name: "int"),
+    args: @[Var(name: "arg",
+                vtyp: Type(name: "int"))])])))
