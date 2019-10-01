69e5dd4a52319bdb4373e6dcd5bd42a334387c29
haxscramper <hax-scamper-git@protonmail.com>
2019-10-01 13:32:13 +0300

    [CLEAN]{PROLOG}

---
+++ testing/prolog/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# bash
+set -o nounset
+set -o errexit
+msg="colecho -b"
+
+swipl -f remove-k-from-n.splog
---
+++ testing/prolog/todo/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# bash
+set -o nounset
+set -o errexit
+# swipl -f sum-neg-pos.splog
