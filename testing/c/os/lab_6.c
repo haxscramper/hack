4364bb1f2ae28cd5e06bf437edefa5b77ce7af48
haxscramper <hax-scamper-git@protonmail.com>
2020-02-23 17:27:38 +0300

    [FEATURE]{C} fork + message queue

--- testing/c/os/lab_6.c
+++ testing/c/os/lab_6.c
@@ -1,8 +1,10 @@
 #include "common.h"
 
+#include <fcntl.h>
 #include <memory.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/signal.h>
@@ -14,7 +16,7 @@
 void test_shared_memory() {
     // Create new memory segment or get access to existing one
     int memory_id = shmget(
-        2, // Segment id 2
+        2, // Segment key 2
         4, // Four bytes
         IPC_CREAT | SHM_R | SHM_W);
     if (memory_id == -1) {
@@ -24,7 +26,8 @@ void test_shared_memory() {
         exit(1);
     }
 
-    if (fork()) {
+    pid_t pid;
+    if (pid = fork()) {
         // Parent process
         milog("Attached parent process to memory segment:", getpid());
         milog("Using memory segnment id:", memory_id);
@@ -40,6 +43,7 @@ void test_shared_memory() {
             // Set value of shared memory segment
             memcpy(str, "hell", 4);
         }
+        waitpid(pid, 0, 0);
     } else {
         // Child process
         milog("Attached child process to memory segment:", getpid());
@@ -53,9 +57,52 @@ void test_shared_memory() {
     }
 }
 
+typedef struct {
+    long msg_type;
+    char msg_buff[6];
+} message_t;
+
+void test_message_queue() {
+
+    // Size of message queue struct minus single field for message
+    // type
+    const size_t msg_size = sizeof(message_t) - sizeof(long);
+
+    // Get message queue id
+    int msg_queue_id = msgget(
+        IPC_PRIVATE, // Create new queue if does not exist
+        IPC_CREAT | 0600);
+    pid_t pid = fork();
+    if (pid > 0) {
+        // Parent process
+        message_t output;
+        output.msg_type = 1;
+        strcpy(output.msg_buff, "123456");
+        if (msgsnd(msg_queue_id, &output, msg_size, 0) == -1) {
+            merr("Failed to add message to queue");
+            mlerr();
+            exit(1);
+        } else {
+            mlog("Wrote message to queue");
+        }
+    } else {
+        // Child process
+        message_t input;
+        int       rcv_stat = 0;
+        msgrcv(msg_queue_id, &input, msg_size, 1, 0);
+        if (rcv_stat < 0) {
+            merr("Failed to read message from queue");
+            mlerr();
+        } else {
+            printf("Recieved message from queue [%s]\n", input.msg_buff);
+            msgctl(msg_queue_id, IPC_RMID, NULL);
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     mlog("Starting lab  6 ");
-    test_shared_memory();
-
+    // test_shared_memory();
+    test_message_queue();
     return 0;
 }
