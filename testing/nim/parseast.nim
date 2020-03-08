d89191ddcd466afebd868b2b63ec3269722336c2
haxscramper <hax-scamper-git@protonmail.com>
2020-03-09 00:20:17 +0300

    [FEATURE]{NIM} Save procedures to sql database

--- testing/nim/parseast.nim
+++ testing/nim/parseast.nim
@@ -11,6 +11,8 @@ import compiler/[
   typesrenderer
 ]
 
+import db_sqlite
+
 import sequtils
 import strutils
 
@@ -32,12 +34,8 @@ type
     name: string
     child: seq[tuple[name, typ: string]]
 
-proc registerProc(n: PNode): void =
-  # for idx, s in n.sons:
-  #   echo idx
-  #   debug s
-
-  let prc = ProcDefintion(
+func registerProc(n: PNode): ProcDefintion =
+  ProcDefintion(
     name: n[0].renderPlainSymbolName(),
     args: n[3].sons
       .filterIt(it.kind == nkIdentDefs)
@@ -48,16 +46,14 @@ proc registerProc(n: PNode): void =
       .join("")
     )
 
-  echo prc
-
 func renderType(t: PNode): string =
   if t.kind == nkBracketExpr:
     "$1[$2]" % [t[0].renderType(), t[1].renderType()]
   else:
     t.renderPlainSymbolName()
 
-proc registerType(n: PNode): void =
-  let tdf = TypeDefinition(
+func registerType(n: PNode): TypeDefinition =
+  TypeDefinition(
     name: n[0].renderPlainSymbolName(),
     child: n[2].sons
       .filterIt(it.kind == nkRecList) # TODO replace with ident definition
@@ -66,17 +62,20 @@ proc registerType(n: PNode): void =
           it.mapIt((name: it[0].renderType, typ: it[1].renderType))
       ).concat()
   )
-  echo tdf
+
+var procs: seq[ProcDefintion]
+var types: seq[TypeDefinition]
 
 proc registerToplevel(n: PNode): void =
   case n.kind:
-    of nkProcDef: registerProc(n)
+    of nkProcDef:
+      procs.add registerProc(n)
     of nkStmtList:
       for s in n.sons: registerTopLevel(s)
     of nkTypeSection:
       for s in n.sons: registerTopLevel(s)
     of nkTypeDef:
-      registerType(n)
+      types.add registerType(n)
     else:
       discard
 
@@ -85,7 +84,7 @@ proc logASTNode(context: PPassContext, n: PNode): PNode =
   result = n
   registerToplevel(n)
 
-proc displayAST*(program: string) =
+proc registerAST*(program: string) =
   let g: ModuleGraph = newModuleGraph(cache, config)
   var m: PSym = makeStdinModule(g)
   incl(m.flags, sfMainModule)
@@ -100,4 +99,34 @@ let thisSource = currentSourcePath().readFile().string()
 #   echo "hi"
 # """)
 
-displayAST(thisSource)
+registerAST(thisSource)
+
+let db = open("database.tmp.db", "", "", "")
+
+const procedure_args = "procedure_args"
+
+db.exec(sql("DROP TABLE IF EXISTS $1" % procedure_args))
+
+db.exec(sql("""
+CREATE TABLE $1 (
+    name VARCHAR(255) NOT NULL,
+    arg VARCHAR(255)
+)""" % procedure_args))
+
+proc printResults(res: seq[seq[string]]): void =
+  let colWidth = res.mapIt(it.mapIt(len(it)).max()).max()
+  for row in res:
+    # let sep = "+" & row.mapIt("-".repeat(colWidth)).join("+") & "+"
+    let line = "| " & row.mapIt(it.alignLeft(colWidth)).join(" | ") & " |"
+    echo line
+
+for pr in procs:
+  for arg in pr.args:
+    let query = "INSERT INTO $1 (name, arg) VALUES (\"$2\", \"$3\")" % [
+      procedure_args, pr.name, arg
+    ]
+    db.exec(sql(query))
+
+printResults db.getAllRows sql("SELECT * FROM $1" % procedure_args)
+
+db.close()
