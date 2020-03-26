f546e37a23298a8f5185347159f2b47d673da562
haxscramper <hax-scamper-git@protonmail.com>
2020-03-26 23:38:00 +0300

    [WIP][FEATURE]{X11} Text drawing example simple

---
+++ testing/x11/interactive-devel.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+export DISPLAY=:0
+Xephyr -resizeable -dpi 40 -sw-cursor -retro -ac :1 &
+export DISPLAY=:1
+
+pid=0
+
+function run() {
+    echo "======================="
+
+    target=$(cat target.txt)
+
+    if ! make --quiet -j12; then
+        colecho -e:0 "Build failed"
+    else
+        colecho -i:0 "Running app"
+        ./$target &
+        pid=$!
+    fi
+    cd ..
+
+    echo "======================="
+}
+
+run
+
+
+echo "monitoring $PWD ..."
+
+inotifywait -m $PWD -e close_write |
+    while read -r directory events filename; do
+        echo "$events $filename"
+        kill $pid
+        run
+    done
+
