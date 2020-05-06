41b22ffc776d4e5e1e6ca1daa3fb5fcaa65a24cc
haxscramper <hax-scamper-git@protonmail.com>
2020-05-06 12:10:29 +0300

    [FEATURE]{WEB} Web form file upload log to spreadsheet

--- testing/web/google_forms/Code.js
+++ testing/web/google_forms/Code.js
@@ -1,18 +1,18 @@
 function doGet(e) {
-    return HtmlService.createHtmlOutputFromFile("forms.html")
-        .setTitle("Google file upload");
+    return HtmlService.createHtmlOutputFromFile('forms.html')
+        .setTitle("Google File Upload by CTRLQ.org");
 }
 
-function hello() {
-    console.log("Hello");
+function msg(message) {
+ Logger.log(message); 
 }
 
 function uploadFileToGoogleDrive(data, file, conf) {
+    Logger.log(conf);
     try {
-        var folder,
-            input = "input",
-            folders = DriveApp.getFoldersByName(input);
 
+        var input = "mirea_input";
+        var folder, folders = DriveApp.getFoldersByName(input);
         Logger.log("Uploading to folder" + input);
 
         if (folders.hasNext()) {
@@ -21,25 +21,43 @@ function uploadFileToGoogleDrive(data, file, conf) {
             folder = DriveApp.createFolder(input);
         }
 
-        var contentType = data.substring(5, data.indexOf(";")),
+        var contentType = data.substring(5, data.indexOf(';')),
             bytes = Utilities.base64Decode(
-                data.substr(data.indexOf("base64,") + 7)),
-            blob = Utilities.newBlob(bytes, contentType, file),
-            name = conf.secondname + "_" + conf.name,
-            filename = name + "_" + conf.group + "_Assing_#" +
-                conf.assign + ".pdf";
+                data.substr(data.indexOf('base64,') + 7)),
+            blob = Utilities.newBlob(bytes, contentType, file);
 
+        var name = conf.secondname + "_" + conf.name;
+        var filename = name + "_" + conf.group + "_Assing_#" +
+                       conf.assign + ".pdf";
 
-        Logger.log("Uploading to folder" + conf.group);
+        Logger.log("Uploading to folder: " + name);
+        var file = folder.createFolder(name).createFile(blob);
+        file.setName(filename);
 
-
-        folder.createFolder(conf.group)
-            .createFolder(name)
-            .createFile(blob)
-            .setName(filename);
-
+        Logger.log("File upload done");
+      
+      var sheet;
+      var files = DriveApp.searchFiles(
+        'starred = true and mimeType = "' + MimeType.GOOGLE_SHEETS + '"');
+      while (files.hasNext()) {
+        var spreadsheet = SpreadsheetApp.open(files.next());
+        sheet = spreadsheet.getSheets()[0];
+        
+        msg(sheet.getName());
+        if (sheet.getName() == "assign") {
+          msg("found 'assign' stared spreadsheet");
+          break; 
+        }
+       
+      }
+      
+      sheet.appendRow([conf.name + " " + conf.secondname, conf.group, conf.assign]);
+      
+      
         return "OK";
+
     } catch (f) {
+        Logger.log(f.toString());
         return f.toString();
     }
 }
