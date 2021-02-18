170bff993e7bfbafadcd8edf6ba9eb164d51ff90
haxscramper <haxscramper@gmail.com>
2021-02-18 23:07:22 +0300

    [FIX] Convert tabs to spaces

--- testing/c/wasm/niminterop/build.sh
+++ testing/c/wasm/niminterop/build.sh
@@ -3,22 +3,6 @@
 set -o errexit
 rm -rf nimcache
 
-# nim c \
-#     -d:asm \
-#     -d:emscripten \
-#     --verbosity:1 \
-#     --cpu:i386 \
-#     --cc:clang \
-#     --clang.exe=emcc \
-#     --clang.linkerexe=emcc \
-# 		--nimcache:nimcache \
-#     --passC:"--shell-file base.html" \
-#     --passC:"-v" \
-#     -o:main.html \
-# 		main.nim
-
-
-
 nim c                      \
     -d:asm                 \
     -d:emscripten          \
@@ -32,24 +16,24 @@ nim c                      \
     --clang.exe=emcc       \
     --clang.linkerexe=emcc \
     --nimcache:nimcache    \
-		--noMain               \
-		--noLinking            \
-		--header:mainlib.h     \
+    --noMain               \
+    --noLinking            \
+    --header:mainlib.h     \
     --gc:arc               \
     main.nim
 
 nimdir=$HOME/.choosenim/toolchains/nim-$(
-		nim --version | grep Version | cut -d' ' -f4 | tr -d '\n')
+        nim --version | grep Version | cut -d' ' -f4 | tr -d '\n')
 
 emcc main.c                                          \
      nimcache/*.c                                    \
-     -O2 -Oz \
+     -O2 -Oz                                         \
      -I$nimdir/lib                                   \
-		 -s EXPORTED_FUNCTIONS="['_printTest', '_main']" \
-		 -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']"   \
+     -s EXPORTED_FUNCTIONS="['_printTest', '_main']" \
+     -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']"   \
      -s ALLOW_MEMORY_GROWTH=1                        \
-		 -s WASM=1                                       \
-	 	 -s NO_EXIT_RUNTIME=1                            \
-		 --shell-file base.html                          \
+     -s WASM=1                                       \
+     -s NO_EXIT_RUNTIME=1                            \
+     --shell-file base.html                          \
      -flto                                           \
-		 -o main.html
+     -o main.html
