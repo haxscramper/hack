a1e6341dc5a7f34744682f4b48818c5a68facd0b
haxscramper <haxscramper@gmail.com>
2021-08-14 20:43:24 +0300

    [CHECKPOINT]

--- testing/nim/compilerapi/nims_template.nim
+++ testing/nim/compilerapi/nims_template.nim
@@ -4,7 +4,7 @@ import
   compiler/[
     ast, pathutils, vm, scriptconfig,
     modulegraphs, options, idents, condsyms, sem, modules,
-    lineinfos, astalgo, parser, vmdef
+    lineinfos, astalgo, vmdef, vmconv
   ]
 
 import
@@ -23,8 +23,17 @@ conf.libpath = AbsoluteDir stdlib
 for p in @[
     stdlib,
     stdlib / "pure",
+    stdlib / "pure" / "collections",
+    stdlib / "pure" / "concurrency",
+    stdlib / "impure",
+    stdlib / "js",
+    stdlib / "packages" / "docutils",
+    stdlib / "std",
     stdlib / "core",
-    stdlib / "pure" / "collections"
+    stdlib / "posix",
+    stdlib / "windows",
+    stdlib / "wrappers",
+    stdlib / "wrappers" / "linenoise"
   ]:
   conf.searchPaths.add(AbsoluteDir p)
 
@@ -32,7 +41,13 @@ conf.cmd = cmdInteractive
 conf.errorMax = high(int)
 conf.structuredErrorHook =
   proc (config: ConfigRef; info: TLineInfo; msg: string; severity: Severity) =
-    assert false, &"{info.line}:{info.col} {msg}"
+    echo &"{info.line}:{info.col} {msg}"
+    assert false
+    # if not (
+    #   "instantiation from here" in msg
+    #   # "type mismatch" in msg
+    # ):
+    #   assert false
 
 
 initDefines(conf.symbols)
@@ -48,13 +63,9 @@ incl(m.flags, sfMainModule)
 graph.vm = setupVM(m, cache, "scriptname.nim", graph)
 graph.compileSystemModule()
 
-proc processModule3(graph: ModuleGraph; module: PSym, n: PNode) =
-  var a: TPassContextArray
-  openPasses(graph, a, module)
-
+proc processModule3(
+    graph: ModuleGraph; module: PSym, n: PNode, a: var TPassContextArray) =
   discard processTopLevelStmt(graph, n, a)
-
-  closePasses(graph, a)
 
 proc getIdent(graph: ModuleGraph, name: string): PNode =
   newIdentNode(graph.cache.getIdent(name), TLineInfo())
@@ -99,6 +110,9 @@ macro pnodeGenRepr*(node: untyped) =
         res.add ($n.kind)[1 .. ^1]
         res.add ")"
 
+      of nnkEmpty:
+        res.add "nkEmpty.newTree()"
+
       else:
         res.add ($n.kind)[1..^1]
         res.add ".newTree(\n"
@@ -107,44 +121,122 @@ macro pnodeGenRepr*(node: untyped) =
           if idx < n.len - 1:
             res.add ",\n"
 
-          # else:
-          #   res.add "\n"
-
-        # res.add "  ".repeat(level)
         res.add ")"
 
   var res: string
   aux(node, res, 1)
   echo res
 
-processModule3(graph, m,
-  nkStmtList.newTree(
-    nkProcDef.newTree(
-      graph.getIdent("customProc"),
-      empty(),
-      empty(),
-      nkFormalParams.newTree(
-        empty(),
-        nkIdentDefs.newTree(graph.getIdent("arg"), graph.getIdent("string"), empty())),
-      empty(),
-      empty(),
-      nkStmtList.newTree(nkDiscardStmt.newTree(empty()))),
-  nkCall.newTree(graph.getIdent("customProc"), newStrNode(nkStrLit, "SSSSSSSSSSSSS"))))
+proc parseNode(graph: ModuleGraph, module: PSym, s: PllStream): PNode =
+  var p: Parser
+  openParser(p, module.fileIdx, s, graph.cache, graph.config)
+
+  result = parseTopLevelStmt(p)
+
+  closeParser(p)
+
+proc processModule2(
+    graph: ModuleGraph; module: PSym,
+    s: PLLStream, a: var TPassContextArray) =
+  var
+    p: Parser
+    fileIdx = module.fileIdx
+
+  openParser(p, fileIdx, s, graph.cache, graph.config)
+
+  while true:
+    if graph.stopCompile(): break
+    var n = parseTopLevelStmt(p)
+    if n.kind == nkEmpty: break
+    if n.kind in imperativeCode:
+      # read everything until the next proc declaration etc.
+      var sl = newNodeI(nkStmtList, n.info)
+      sl.add n
+      var rest: PNode = nil
+      while true:
+        var n = parseTopLevelStmt(p)
+        if n.kind == nkEmpty or n.kind notin imperativeCode:
+          rest = n
+          break
+        sl.add n
+      if not processTopLevelStmt(graph, sl, a): break
+      if rest != nil:
+        if not processTopLevelStmt(graph, rest, a): break
+
+    else:
+      if not processTopLevelStmt(graph, n, a): break
+
+var a: TPassContextArray
+openPasses(graph, a, m)
+
+processModule2(graph, m, llStreamOpen("""
+echo "Evaluated thing with template"
+echo "second statement"
+
+template testTemplate(): untyped =
+  echo "expanded template"
+
+template templateExecutedInVm*(): untyped =
+  result &= "example"   # <-  vm
+  echo "RESULT: ", result
+  echo "IN macroexpanded code ii: ", ii, " ss: ", ss
+  if ii == 1:   # <-  vm
+    echo "Added to result"
+    result &= ss   # <-  vm
+    echo "RESULT: ", result
+  var myvar = 1   # <-  vm
+  inc(myvar, 1)   # <-  vm
+
+testTemplate()
+
+proc setOutResult[T](arg: T) = discard
+
+"""), a)
+
+processModule2(graph, m, llStreamOpen("testTemplate()"), a)
 
 
-pnodeGenRepr:
-  for i in 0 .. 12:
-    echo i
+proc runtimeImplBody(ss: string, ii: int): string =
+  var fromVm: string
 
-processModule3(graph, m,
-  nkStmtList.newTree(
-    nkForStmt.newTree(
-      graph.getIdent("i"),
-      nkInfix.newTree(
-        graph.getIdent(".."),
-        newIntNode(nkIntLit, 0),
-        newIntNode(nkIntLit, 12)),
+  graph.vm.PEvalContext().registerCallback(
+    "setOutResult",
+    proc(args: VmArgs) =
+      echo "Setting value from the proc implementation"
+      fromVm = args.getString(0)
+      echo "Accepted value from the VM")
+
+  processModule3(graph, m,
+    nkBlockStmt.newTree(nkEmpty.newTree(),
       nkStmtList.newTree(
-        nkCommand.newTree(
-          graph.getIdent("echo"),
-          graph.getIdent("i"))))))
+        # Pass arguments to the environemtn
+        nkLetSection.newTree(
+          nkIdentDefs.newTree(graph.getIdent("ss"), nkEmpty.newTree(), vmconv.toLit(ss)),
+          nkIdentDefs.newTree(graph.getIdent("ii"), nkEmpty.newTree(), vmconv.toLit(ii))),
+
+        # Prepare pseudo-function environment
+        nkVarSection.newTree(
+          nkIdentDefs.newTree(
+            graph.getIdent("result"),
+            graph.getIdent("string"),
+            nkEmpty.newTree())),
+
+        # # Execute macro call
+        # nkCall.newTree(graph.getIdent("templateExecutedInVm")),
+        parseNode(graph, m, llStreamOpen("templateExecutedInVm()")),
+
+        # Actually pass result to compiled part
+        nkCall.newTree(
+          graph.getIdent("setOutResult"),
+          graph.getIdent("result")))), a)
+
+
+  return fromVm
+
+echo "-------- FIRST CALL ------------"
+echo "[== ", runtimeImplBody("123", 12), " ==]"
+
+echo "-------- SECOND CALL ------------"
+echo "[== ", runtimeImplBody("123", 1), " ==]"
+
+closePasses(graph, a)
