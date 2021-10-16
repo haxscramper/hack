39af0c89d4c68264253df4bdbcc97fdab67db83a
haxscramper <haxscramper@gmail.com>
2021-10-16 14:23:03 +0300

    [CHECKPOINT] Odt generator is even more disgusting than libreshit itself, so I don't think it is worth it

--- testing/nim/implementation_ideas/generate_odt/generate_odt.nim
+++ testing/nim/implementation_ideas/generate_odt/generate_odt.nim
@@ -11,12 +11,15 @@ let contentXml: string = readFile(relToSource"content.xml")
 type
   OdtWriter = object
     writer: XmlWriter
+    resDir: AbsDir
 
 using
   writer: var OdtWriter
   params: openarray[(string, string)]
 
-proc newOdtWriter*(): OdtWriter = OdtWriter(writer: newXmlWriter())
+proc newOdtWriter*(dir: AbsDir): OdtWriter =
+  OdtWriter(writer: newXmlWriter(), resDir: dir)
+
 proc readAll*(writer): string = writer.writer.readAll()
 
 proc raw(writer; text: string) = writer.writer.writeRaw(text)
@@ -24,20 +27,70 @@ proc raw(writer; text: string) = writer.writer.writeRaw(text)
 proc eopen(writer; elem: string, params; indent: bool = false) =
   writer.writer.xmlStart(elem, params, indent)
 
+proc esingle(writer; elem: string, params; indent: bool = false) =
+  writer.writer.xmlSingle(elem, params, indent)
+
 proc eclose(writer; elem: string; indent: bool = true) =
   writer.writer.xmlEnd(elem, indent)
 
-template p(writer; params; body: untyped): untyped =
-  eopen(writer, "text:p", params, false)
+template ewrap(writer; name: string; params; body: untyped): untyped =
+  eopen(writer, name, params, false)
   body
-  eclose(writer, "text:p")
+  eclose(writer, name)
+
+template ewrapl(writer; name: string; params; body: untyped): untyped =
+  eopen(writer, name, params, true)
+  body
+  eclose(writer, name, true)
+
+template ewrapl1(writer; name: string; params; body: untyped): untyped =
+  eopen(writer, name, params, false)
+  body
+  eclose(writer, name, true)
+
+proc cpFile(writer; file: AbsFile, sub: string) =
+  let dir = writer.resDir / RelDir(sub)
+  mkDir(dir)
+  cpFile(file, dir /. file.splitFile2().file)
+
+proc line(writer) = writer.writer.line()
+
+proc image(writer; file: AbsFile) =
+  assertExists file
+  writer.ewrapl("draw:frame", {
+    "text:anchor-type": "as-char",
+    "svg:width": "6.9252in",
+    "svg:height": "3.8972in",
+    "draw:z-index": "0"
+  }):
+    writer.esingle("draw:image", {
+      "xlink:href": "Pictures/" & file.name(),
+      "xlink:type": "simple",
+      "xlink:show": "embed",
+      "xlink:actuate": "onLoad",
+      "draw:mime-type": "image/jpeg"
+    })
+
+    writer.cpFile(file, "Pictures")
+
+
+template p(writer; params; body: untyped): untyped =
+  ewrap(writer, "text:p", params, body)
+  # eopen(writer, "text:p", params)
+  # body
+  # eclose(writer, "text:p")
 
 template span(writer; params; body: untyped): untyped =
-  eopen(writer, "text:span", params, false)
-  body
-  eclose(writer, "text:span", false)
+  ewrap(writer, "text:span", params, body)
+  # eopen(writer, "text:span", params, false)
+  # body
+  # eclose(writer, "text:span", false)
 
-var w = newOdtWriter()
+let dir = getAppTempDir()
+rmDir dir
+
+
+var w = newOdtWriter(dir)
 for (kind, value) in interpolatedExprs(contentXml):
   case kind:
     of iekVar:
@@ -50,7 +103,11 @@ for (kind, value) in interpolatedExprs(contentXml):
               w.raw("123")
 
           w.p({"style-name": "P1"}):
-            w.raw("Secon paragraph in the document")
+            w.raw("Secon paragrasfdasdfin the document")
+
+          # w.p({"style-name": "P1"}):
+          #   w.span({"style-name": "T1"}):
+          #     w.image(AbsFile"/tmp/file.jpg")
 
 
         else:
@@ -63,8 +120,6 @@ for (kind, value) in interpolatedExprs(contentXml):
     else:
       w.raw value
 
-let dir = getAppTempDir()
-rmDir dir
 
 mkWithDirStructure dir:
   dir "META-INF":
