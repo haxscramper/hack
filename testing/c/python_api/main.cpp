1c25d857e9ab4469f9d1abd84413a609fce2c37d
haxscramper <haxscramper@gmail.com>
2022-06-10 15:15:09 +0300

    check

--- testing/c/python_api/main.cpp
+++ testing/c/python_api/main.cpp
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include <frameobject.h>
 
 // HACK because python API does not allow for custom user data passing, so
 // I have to
@@ -13,21 +14,46 @@ void eval(const char* text) {
 
 static PyObject* triggerFromPy(PyObject* self, PyObject* args) {
     puts("Function called from the python code");
+
+    if (self == nullptr) {
+        puts("SELF IS NIL");
+    } else {
+        printf("SELF: %s\n", _PyUnicode_AsString(PyObject_Repr(self)));
+
+    }
+
+    PyThreadState* ts    = PyThreadState_Get();
+    PyFrameObject* frame = ts->frame;
+    while (frame != 0) {
+        char const* filename = _PyUnicode_AsString(
+            frame->f_code->co_filename);
+        char const* name = _PyUnicode_AsString(frame->f_code->co_name);
+        printf("called: filename=%s, name=%s\n", filename, name);
+        frame = frame->f_back;
+    }
+
     return Py_None;
 }
 
+/// Wrapper around dynamically allocated array of python methods. Used to
+/// properly manage addition of new methods to the list. Use of std::vector
+/// was not possible due to potential data block reallocations, which
+/// cannot be accounted for in the Python API
 struct MethodTable {
     PyMethodDef* methods;
-    int          idx = 0;
-    const int    size = 256;
-    MethodTable() {
+    int          idx  = 0;
+    const int    size = 256; /// Hardcoded array size, cannot be changed
+        /// dynamically due to aforementioned constraints
+        /// on reallocation.
+    inline MethodTable() {
         methods = new PyMethodDef[size]();
     }
-    void add(PyMethodDef def) {
+    /// Add new method to the method table
+    inline void add(PyMethodDef def) {
         methods[idx] = def;
         ++idx;
     }
-    ~MethodTable() {
+    inline ~MethodTable() {
         delete[] methods;
     }
 };
