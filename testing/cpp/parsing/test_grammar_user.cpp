64229c25fd13443469329f096fc71beb5c539a0c
haxscramper <haxscramper@gmail.com>
2022-12-30 11:13:46 +0400

    use custom lexer for antlr4

--- testing/cpp/parsing/test_grammar_user.cpp
+++ testing/cpp/parsing/test_grammar_user.cpp
@@ -27,6 +27,7 @@ class MyParserErrorListener : public antlr4::BaseErrorListener
 
 
 void treeRepr(
+    std::ostream&             os,
     const test_grammarParser& parser,
     tree::ParseTree*          tree,
     int                       level) {
@@ -42,7 +43,7 @@ void treeRepr(
             Token* start     = parserContext->getStart();
             Token* end       = parserContext->getStart();
 
-            std::cout <<                              //
+            os <<                                     //
                 start->getLine() << ":" <<            //
                 start->getCharPositionInLine() <<     //
                 std::string(2 * level, ' ') << " " << //
@@ -50,28 +51,59 @@ void treeRepr(
 
             for (int i = 0; i < tree->children.size(); ++i) {
                 std::cout << "\n";
-                treeRepr(parser, tree->children.at(i), level + 1);
+                treeRepr(os, parser, tree->children.at(i), level + 1);
             }
             break;
         }
         case tree::ParseTreeType::TERMINAL: {
-            std::cout << std::string(2 * level + 2, ' ') << " '"
-                      << tree->getText() << "'";
+            os << std::string(2 * level + 2, ' ') << " '"
+               << tree->getText() << "'";
             break;
         }
         case tree::ParseTreeType::ERROR: {
-            std::cout << " ERROR";
+            os << " ERROR";
             break;
         }
     }
 }
+
+struct CustomLexer : Lexer
+{
+    test_grammarLexer* base;
+    CustomLexer(test_grammarLexer* _lexer) : base(_lexer) {}
+
+    std::unique_ptr<Token> nextToken() override {
+        auto result = base->nextToken();
+        return result;
+    }
+
+    // Passthrough for the lexer API, these things might be genrated
+    // automatically later on.
+    std::vector<std::string> const& getRuleNames() const override {
+        return base->getRuleNames();
+    }
+    dfa::Vocabulary const& getVocabulary() const override {
+        return base->getVocabulary();
+    }
+    std::string getGrammarFileName() const override {
+        return base->getGrammarFileName();
+    }
+    const atn::ATN& getATN() const override { return base->getATN(); }
+    const std::vector<std::string>& getChannelNames() const override {
+        return base->getChannelNames();
+    }
+    const std::vector<std::string>& getModeNames() const override {
+        return base->getModeNames();
+    }
+};
 
 int main(int argc, char* argv[]) {
     std::ifstream file{};
     file.open(argv[1]);
     ANTLRInputStream  input{file};
     test_grammarLexer lexer{&input};
-    CommonTokenStream tokens{&lexer};
+    CustomLexer       custom{&lexer};
+    CommonTokenStream tokens{&custom};
 
     MyParserErrorListener errorListner;
 
@@ -83,12 +115,11 @@ int main(int argc, char* argv[]) {
     try {
         tree::ParseTree* tree = parser.main();
         std::cout << tree->toStringTree() << std::endl;
-        treeRepr(parser, tree, 0);
+        treeRepr(std::cout, parser, tree, 0);
+        std::cout << "\ndone" << std::endl;
         return 0;
     } catch (std::invalid_argument& e) {
         std::cout << e.what() << std::endl;
         return 10;
     }
-
-    std::cout << "done" << std::endl;
 }
