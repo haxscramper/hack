eb61c9898c6d80af4faf8c5297afceeed3912e76
haxscramper <haxscramper@gmail.com>
2023-06-24 11:46:25 +0400

    Zoomable timeline for d3.js v7

--- testing/cpp/haxorg_tests/sem_id_recasting.cpp
+++ testing/cpp/haxorg_tests/sem_id_recasting.cpp
@@ -9,28 +9,28 @@ using uint8_t  = unsigned char;
 template <typename T>
 using Vec = std::vector<T>;
 
-enum class SemOrgKind : uint8_t
+enum class OrgKind : uint8_t
 {
     Word,
     Space,
     Subtree
 };
 
-struct SemOrg;
+struct Org;
 
-struct SemId {
+struct OrgId {
     uint64_t id = 0;
     bool     isNil() const { return id == 0; }
 
-    static SemId Nil() { return SemId(0, SemOrgKind(0), 0); }
+    static OrgId Nil() { return OrgId(0, OrgKind(0), 0); }
 
-    SemId(uint32_t storeIndex, SemOrgKind kind, uint32_t nodeIndex) {
+    OrgId(uint32_t storeIndex, OrgKind kind, uint32_t nodeIndex) {
         setStoreIndex(storeIndex);
         setKind(kind);
         setNodeIndex(nodeIndex);
     }
 
-    SemOrgKind getKind() const { return SemOrgKind((id >> 32) & 0xFF); }
+    OrgKind getKind() const { return OrgKind((id >> 32) & 0xFF); }
 
     uint32_t getNodeIndex() const {
         assert(!isNil());
@@ -43,7 +43,7 @@ struct SemId {
         id = (id & 0x000000FFFFFFFFFF) | ((uint64_t)storeIndex << 40);
     }
 
-    void setKind(SemOrgKind kind) {
+    void setKind(OrgKind kind) {
         id = (id & 0xFFFFFF00FFFFFFFF) | ((uint64_t)kind << 32);
     }
 
@@ -51,45 +51,47 @@ struct SemId {
         id = (id & 0xFFFFFFFF00000000) | (nodeIndex + 1);
     }
 
-    SemOrg& get();
+    Org& get();
 };
 
 template <typename T>
-struct SemKindId : public SemId {
-    SemId toId() const { return *this; }
-    SemKindId(SemId base) : SemId(base) {}
+struct SemKindId : public OrgId {
+    OrgId toId() const { return *this; }
+    SemKindId(OrgId base) : OrgId(base) {}
 
     T* operator->();
 };
 
-struct SemOrg {
-    virtual SemOrgKind getKind() = 0;
-    SemId              parent;
-    Vec<SemId>         nested;
-    SemOrg(SemId parent) : parent(parent) {}
+struct Org {
+    virtual OrgKind getKind() = 0;
+    OrgId           parent;
+    Vec<OrgId>      nested;
+    Org(OrgId parent) : parent(parent) {}
+    void  push_back(OrgId sub) { nested.push_back(sub); }
+    OrgId at(int index) { return nested.at(index); }
 };
 
-struct Word : SemOrg {
-    using SemOrg::SemOrg;
-    virtual SemOrgKind getKind() override { return SemOrgKind::Word; }
+struct Word : Org {
+    using Org::Org;
+    virtual OrgKind getKind() override { return OrgKind::Word; }
 
-    static const SemOrgKind staticKind = SemOrgKind::Word;
-    static SemKindId<Word>  create(SemId parent);
+    static const OrgKind   staticKind = OrgKind::Word;
+    static SemKindId<Word> create(OrgId parent);
 };
 
-struct Space : SemOrg {
-    using SemOrg::SemOrg;
-    virtual SemOrgKind getKind() override { return SemOrgKind::Space; }
-    static const SemOrgKind staticKind = SemOrgKind::Space;
+struct Space : Org {
+    using Org::Org;
+    virtual OrgKind      getKind() override { return OrgKind::Space; }
+    static const OrgKind staticKind = OrgKind::Space;
 
-    static SemKindId<Space> create(SemId parent);
+    static SemKindId<Space> create(OrgId parent);
 };
 
-struct Subtree : SemOrg {
-    using SemOrg::SemOrg;
-    virtual SemOrgKind getKind() override { return SemOrgKind::Subtree; }
-    static const SemOrgKind   staticKind = SemOrgKind::Subtree;
-    static SemKindId<Subtree> create(SemId parent);
+struct Subtree : Org {
+    using Org::Org;
+    virtual OrgKind      getKind() override { return OrgKind::Subtree; }
+    static const OrgKind staticKind = OrgKind::Subtree;
+    static SemKindId<Subtree> create(OrgId parent);
 };
 
 template <typename T>
@@ -98,8 +100,8 @@ struct KindStore {
 
     T& getForIndex(uint32_t index) { return values.at(index); }
 
-    SemId create(uint32_t selfIndex, SemId parent) {
-        SemId result = SemId(selfIndex, T::staticKind, values.size());
+    OrgId create(uint32_t selfIndex, OrgId parent) {
+        OrgId result = OrgId(selfIndex, T::staticKind, values.size());
         values.emplace_back(parent);
         return result;
     }
@@ -111,20 +113,19 @@ struct LocalStore {
     KindStore<Space>   spaces;
     KindStore<Subtree> subtrees;
 
-    SemOrg& get(SemOrgKind kind, uint32_t index) {
+    Org& get(OrgKind kind, uint32_t index) {
         switch (kind) {
-            case SemOrgKind::Word: return words.getForIndex(index);
-            case SemOrgKind::Space: return spaces.getForIndex(index);
-            case SemOrgKind::Subtree: return subtrees.getForIndex(index);
+            case OrgKind::Word: return words.getForIndex(index);
+            case OrgKind::Space: return spaces.getForIndex(index);
+            case OrgKind::Subtree: return subtrees.getForIndex(index);
         }
     }
 
-    SemId create(uint32_t selfIndex, SemOrgKind kind, SemId parent) {
+    OrgId create(uint32_t selfIndex, OrgKind kind, OrgId parent) {
         switch (kind) {
-            case SemOrgKind::Word: return words.create(selfIndex, parent);
-            case SemOrgKind::Space:
-                return spaces.create(selfIndex, parent);
-            case SemOrgKind::Subtree:
+            case OrgKind::Word: return words.create(selfIndex, parent);
+            case OrgKind::Space: return spaces.create(selfIndex, parent);
+            case OrgKind::Subtree:
                 return subtrees.create(selfIndex, parent);
         }
     }
@@ -139,7 +140,7 @@ class GlobalStore {
 
     LocalStore& getStoreByIndex(int index) { return stores.at(index); }
 
-    static SemId createIn(uint32_t index, SemOrgKind kind, SemId parent) {
+    static OrgId createIn(uint32_t index, OrgKind kind, OrgId parent) {
         return getInstance().getStoreByIndex(index).create(
             index, kind, parent);
     }
@@ -153,7 +154,7 @@ class GlobalStore {
 };
 
 
-SemOrg& SemId::get() {
+Org& OrgId::get() {
     return GlobalStore::getInstance()
         .getStoreByIndex(getStoreIndex())
         .get(getKind(), getNodeIndex());
@@ -164,23 +165,24 @@ T* SemKindId<T>::operator->() {
     return static_cast<T*>(&get());
 }
 
-SemKindId<Space> Space::create(SemId parent) {
-    return GlobalStore::createIn(0, SemOrgKind::Space, parent);
+SemKindId<Space> Space::create(OrgId parent) {
+    return GlobalStore::createIn(0, OrgKind::Space, parent);
 }
 
-SemKindId<Word> Word::create(SemId parent) {
-    return GlobalStore::createIn(0, SemOrgKind::Word, parent);
+SemKindId<Word> Word::create(OrgId parent) {
+    return GlobalStore::createIn(0, OrgKind::Word, parent);
 }
 
-SemKindId<Subtree> Subtree::create(SemId parent) {
-    return GlobalStore::createIn(0, SemOrgKind::Subtree, parent);
+SemKindId<Subtree> Subtree::create(OrgId parent) {
+    return GlobalStore::createIn(0, OrgKind::Subtree, parent);
 }
 
 int main() {
     GlobalStore::getInstance().stores.push_back(LocalStore());
-    SemKindId<Subtree> tree = Subtree::create(SemId::Nil());
+    SemKindId<Subtree> tree = Subtree::create(OrgId::Nil());
     SemKindId<Word>    word = Word::create(tree);
 
 
+    std::cout << "ok\n";
     return 0;
 }
