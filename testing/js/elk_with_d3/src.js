bb9ca3fb1a45e71a70944e0a291f6125ff78df53
haxscramper <haxscramper@gmail.com>
2023-08-06 18:59:29 +0400

    Correct misaligned node translates

--- testing/js/elk_with_d3/src.js
+++ testing/js/elk_with_d3/src.js
@@ -26,99 +26,120 @@ function toSvgPaths(edge) {
   });
 }
 
-function renderGraph(svg, root, scaleFactor = 1.0) {
-  const styles = defaultStyles();
-  svg.attr("width", root.data.width).attr("height", root.data.height);
+const styles = defaultStyles();
 
-  // Define edge arrow marker
-  const defs = svg.append("defs");
-  defs.append("marker")
-      .attr("id", "edgeShapeMarker")
-      .attr("markerWidth", 10)
-      .attr("markerHeight", 10)
-      .attr("refX", 6)
-      .attr("refY", 3)
-      .attr("orient", "auto")
-      .attr("markerUnits", "strokeWidth")
-      .append("path")
-      .attr("d", "M0,0 L0,6 L6,3 z")
-      .attr("fill", styles.edgeShapeStroke);
+function drawEdge(parent, edge) {
+  const edgePaths = toSvgPaths(edge);
 
-  function drawEdge(parent, edge) {
-    const edgePaths = toSvgPaths(edge);
+  edgePaths.forEach(edgePathString => {
+    parent.append("path")
+        .attr("d", edgePathString)
+        .attr("stroke", styles.edgeShapeStroke)
+        .attr("fill", styles.edgeShapeFill)
+        .attr("marker-end", "url(#edgeShapeMarker)");
+  });
+}
 
-    edgePaths.forEach(edgePathString => {
-      parent.append("path")
-          .attr("d", edgePathString)
-          .attr("stroke", styles.edgeShapeStroke)
-          .attr("fill", styles.edgeShapeFill)
-          .attr("marker-end", "url(#edgeShapeMarker)");
+function drawNode(parent, node) {
+  // Create a group for each node
+  const g = parent.append("g").attr(
+      "transform", `translate(${node.parent.data.x}, ${node.parent.data.y})`);
+  g.appendComment("Node");
+
+  // Add a rectangle for the node
+  g.append("rect")
+      .attr("x", node.data.x)
+      .attr("y", node.data.y)
+      .attr("width", node.data.width)
+      .attr("height", node.data.height)
+      .attr("stroke", styles.nodeShapeStroke)
+      .attr("fill", styles.nodeShapeFill);
+
+  const labels = g.append("g");
+  labels.appendComment("Title");
+
+  // Add labels
+  if (node.data.labels) {
+    node.data.labels.forEach((label) => {
+      labels.append("text")
+          .attr("x", node.data.x + label.x)
+          .attr("y", node.data.y + label.y)
+          .attr("stroke", styles.nodeLabelStroke)
+          .attr("fill", styles.nodeLabelFill)
+          .style("font",
+                 `${styles.nodeLabelFontSize} ${styles.nodeLabelFontFamily}`)
+          .attr("text-anchor", "left")
+          .attr("alignment-baseline", "hanging")
+          .text(label.text);
     });
   }
 
-  function drawNode(parent, node) {
-    // Create a group for each node
-    const g = parent.append("g").attr(
-        "transform", `translate(${node.data.x}, ${node.data.y})`);
-    g.appendComment("Node");
+  const ports = g.append("g");
+  ports.appendComment("Ports");
 
-    // Add a rectangle for the node
-    g.append("rect")
-        .attr("x", node.data.x)
-        .attr("y", node.data.y)
-        .attr("width", node.data.width)
-        .attr("height", node.data.height)
-        .attr("stroke", styles.nodeShapeStroke)
-        .attr("fill", styles.nodeShapeFill);
-
-    // Add labels
-    if (node.data.labels) {
-      node.data.labels.forEach((label) => {
-        g.append("text")
-            .attr("x", node.data.x + label.x)
-            .attr("y", node.data.y + label.y)
-            .attr("stroke", styles.nodeLabelStroke)
-            .attr("fill", styles.nodeLabelFill)
-            .style("font",
-                   `${styles.nodeLabelFontSize} ${styles.nodeLabelFontFamily}`)
-            .attr("text-anchor", "left")
-            .attr("alignment-baseline", "hanging")
-            .text(label.text);
-      });
-    }
-
-    // Add ports
-    if (node.data.ports) {
-      node.data.ports.forEach(function(port) {
-        g.appendComment("Port");
-        g.append("rect")
-            .attr("x", node.data.x + port.x)
-            .attr("y", node.data.y + port.y)
-            .attr("width", port.width)
-            .attr("height", port.height)
-            .attr("stroke", styles.portShapeStroke)
-            .attr("fill", styles.portShapeFill);
-      });
-    }
-
-    if (node.data.edges) {
-      node.data.edges.forEach(edge => drawEdge(g, edge));
-    }
-
-    // Draw child nodes recursively
-    if (node.children) {
-      node.children.forEach(node => drawNode(g, node));
-    }
+  // Add ports
+  if (node.data.ports) {
+    node.data.ports.forEach(function(port) {
+      ports.append("rect")
+          .attr("x", node.data.x + port.x)
+          .attr("y", node.data.y + port.y)
+          .attr("width", port.width)
+          .attr("height", port.height)
+          .attr("stroke", styles.portShapeStroke)
+          .attr("fill", styles.portShapeFill);
+    });
   }
-  // Draw the root node
-  drawNode(svg, root);
+
+  const edges = g.append("g").attr("transform",
+                                   `translate(${node.data.x}, ${node.data.y})`);
+  edges.appendComment("Edges");
+
+  if (node.data.edges) {
+    node.data.edges.forEach(edge => drawEdge(edges, edge));
+  }
+
+  const nodes = g.append("g");
+  nodes.appendComment("Nodes");
+
+  // Draw child nodes recursively
+  if (node.children) {
+    node.children.forEach(node => drawNode(nodes, node));
+  }
 }
 
 function update(graph) {
   const elk = new ELK()
   elk.layout(graph).then(function(layout) {
-    console.log(layout);
-    renderGraph(d3.select("svg"), d3.hierarchy(layout, d => d.children));
+    var root = d3.hierarchy(layout, d => d.children);
+    var svg = d3.select("svg");
+    console.log(root);
+
+    svg.attr("width", root.data.width)
+        .attr("height", root.data.height)
+        .attr("viewBox", `${root.data.x} ${root.data.y} ${root.data.width} ${
+                             root.data.height}`);
+
+    // Define edge arrow marker
+    const defs = svg.append("defs");
+    defs.append("marker")
+        .attr("id", "edgeShapeMarker")
+        .attr("markerWidth", 10)
+        .attr("markerHeight", 10)
+        .attr("refX", 6)
+        .attr("refY", 3)
+        .attr("orient", "auto")
+        .attr("markerUnits", "strokeWidth")
+        .append("path")
+        .attr("d", "M0,0 L0,6 L6,3 z")
+        .attr("fill", styles.edgeShapeStroke);
+
+    for (var node of root.children) {
+      drawNode(svg, node);
+    }
+
+    for (var edge of root.data.edges) {
+      drawEdge(svg, edge);
+    }
   });
 }
 
