978c6cdc2b730ce8c00fbc84c52463ecec386fd5
haxscramper <haxscramper@gmail.com>
2026-03-31 16:43:44 +0400

    merge document on different thread

--- testing/python/pdf_converter/gui/right_panel.py
+++ testing/python/pdf_converter/gui/right_panel.py
@@ -2,8 +2,21 @@ import logging
 from typing import Optional, List
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QTextBrowser, QCheckBox, QHBoxLayout
+from PySide6.QtCore import QThread, Signal
 from models import PageData, DocTag
 from html_generator import generate_html_content, map_tag_to_html
+
+class HtmlGeneratorThread(QThread):
+    html_ready = Signal(str)
+
+    def __init__(self, pages_data: List[PageData], use_llm: bool, parent=None):
+        super().__init__(parent)
+        self.pages_data = pages_data
+        self.use_llm = use_llm
+
+    def run(self):
+        final_html = generate_html_content(self.pages_data, use_llm=self.use_llm)
+        self.html_ready.emit(final_html)
 
 class RightPanel(QWidget):
     def __init__(self, parent: Optional[QWidget] = None) -> None:
@@ -45,7 +58,16 @@ class RightPanel(QWidget):
         return mapping.get(tag_name, "div")
 
     def generate_html(self, pages_data: List[PageData]) -> None:
-        """Generates simple mechanical HTML from the parsed blocks."""
+        """Generates HTML from the parsed blocks in a separate thread."""
         use_llm = self.use_llm_checkbox.isChecked()
-        final_html = generate_html_content(pages_data, use_llm=use_llm)
+        self.update_btn.setEnabled(False)
+        self.html_view.setHtml("<html><body><p>Generating HTML... Please wait.</p></body></html>")
+        
+        self.html_thread = HtmlGeneratorThread(pages_data, use_llm)
+        self.html_thread.html_ready.connect(self.on_html_ready)
+        self.html_thread.start()
+
+    def on_html_ready(self, final_html: str) -> None:
         self.html_view.setHtml(final_html)
+        self.update_btn.setEnabled(True)
+        self.html_thread = None
